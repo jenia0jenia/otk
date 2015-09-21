77edc4d68e4947a931de0f3a9b0723f37e0fefcc
jenia0jenia <jenia0jenia@gmail.com>
2015-09-22 02:13:38 +0500

    frst commit

---
+++ shop/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'kzyw0bF5xm5ZopJ1ISZuwW3jXqZXO0PszIkdDgi0gykwAIaFREpSdFMV');
+define('_COOKIE_IV_', 'M5iipJOQ');
+define('_PS_CREATION_DATE_', '2015-09-08');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.1');
+define('_RIJNDAEL_KEY_', '10MNdmwwzN9xxYEh8xt1vctdyPIT2kMV');
+define('_RIJNDAEL_IV_', 'Jwd+JjzJoZbHad6WcCudLQ==');
